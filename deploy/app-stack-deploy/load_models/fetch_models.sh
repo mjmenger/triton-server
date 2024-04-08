22027c8d399498e56144c62a9b9ff2da7c0f35ee
Mark J Menger <mjmenger@users.noreply.github.com>
2024-04-08 19:41:15 +0000

    fix inception model config

--- deploy/app-stack-deploy/load_models/fetch_models.sh
+++ deploy/app-stack-deploy/load_models/fetch_models.sh
@@ -27,16 +27,30 @@
 
 set -ex
 
+echo "LOAD_INCEPTION_GRAPHDEF: $LOAD_INCEPTION_GRAPHDEF"
+echo "LOAD_DENSENET_ONNX: $LOAD_DENSENET_ONNX"
+
 cd /models
 
-# TensorFlow inception
-mkdir -p model_repository/inception_graphdef/1
-wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
-     https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
-(cd /tmp && tar xzf inception_v3_2016_08_28_frozen.pb.tar.gz)
-mv /tmp/inception_v3_2016_08_28_frozen.pb model_repository/inception_graphdef/1/model.graphdef
+if [ "$LOAD_INCEPTION_GRAPHDEF" -eq 1 ]
+then
+     
+     echo "Loading TensorFlow inception"
+     mkdir -p model_repository/inception_graphdef/1
+     wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
+          https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
+     (cd /tmp && tar xzf inception_v3_2016_08_28_frozen.pb.tar.gz)
+     mv /tmp/inception_v3_2016_08_28_frozen.pb model_repository/inception_graphdef/1/model.graphdef
+     wget -O model_repository/inception_graphdef/config.pbtxt \
+          https://raw.githubusercontent.com/triton-inference-server/server/main/docs/examples/model_repository/inception_graphdef/config.pbtxt
+     wget -O model_repository/inception_graphdef/inception_labels.txt \
+          https://raw.githubusercontent.com/triton-inference-server/server/main/docs/examples/model_repository/inception_graphdef/inception_labels.txt
+fi
 
-# ONNX densenet
-mkdir -p model_repository/densenet_onnx/1
-wget -O model_repository/densenet_onnx/1/model.onnx \
-     https://contentmamluswest001.blob.core.windows.net/content/14b2744cf8d6418c87ffddc3f3127242/9502630827244d60a1214f250e3bbca7/08aed7327d694b8dbaee2c97b8d0fcba/densenet121-1.2.onnx
+if [ "$LOAD_DENSENET_ONNX" -eq 1 ]
+then
+     echo "Loading ONNX densenet"
+     mkdir -p model_repository/densenet_onnx/1
+     wget -O model_repository/densenet_onnx/1/model.onnx \
+          https://contentmamluswest001.blob.core.windows.net/content/14b2744cf8d6418c87ffddc3f3127242/9502630827244d60a1214f250e3bbca7/08aed7327d694b8dbaee2c97b8d0fcba/densenet121-1.2.onnx
+fi
